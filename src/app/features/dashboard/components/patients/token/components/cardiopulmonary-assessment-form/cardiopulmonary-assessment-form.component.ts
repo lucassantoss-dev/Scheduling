a810ae777d2cd5cde62993965db6801c8e1731ad
lucassantossdev <lucas.santos@severinoapp.com>
2024-04-10 19:07:30 -0300

    feat: change token forms

--- src/app/features/dashboard/components/patients/token/components/cardiopulmonary-assessment-form/cardiopulmonary-assessment-form.component.ts
+++ src/app/features/dashboard/components/patients/token/components/cardiopulmonary-assessment-form/cardiopulmonary-assessment-form.component.ts
@@ -8,6 +8,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class CardiopulmonaryAssessmentFormComponent implements OnInit {
 	public formulario!: FormGroup;
+	public firstScreen: boolean = false;
 	constructor(
 		private formBuilder: FormBuilder,
 	) {
@@ -17,6 +18,19 @@ export class CardiopulmonaryAssessmentFormComponent implements OnInit {
 		this.criarFormulario();
 	}
 
+	nextStep() {
+		this.firstScreen = false;
+	}
+
+	createAssessment(): void {
+		const formvalue = Object.assign({}, this.formulario.getRawValue());
+		console.log('formValue', formvalue);
+	}
+
+	backStep() {
+		this.firstScreen = true;
+	}
+
 	private criarFormulario(): void {
 		this.formulario = this.formBuilder.group({
 			static_inspection: new FormControl({ value: '', disabled: false }, [Validators.required]),
@@ -24,6 +38,23 @@ export class CardiopulmonaryAssessmentFormComponent implements OnInit {
 			fc: new FormControl({ value: '', disabled: false }, [Validators.required]),
 			f: new FormControl({ value: '', disabled: false }, [Validators.required]),
 			weight: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			height: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			breathing_rhythm: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			secretion: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			imc: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			sat: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			hydration: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			cyanosis: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			perfusion: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			jaundice: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			entry_way: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			breathing_pattern: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			breathing_range: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			inspiration_exhalation: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			cough: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			cough_second: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			lung_auscultation: new FormControl({ value: '', disabled: false }, [Validators.required]),
+			obs: new FormControl({ value: '', disabled: false }, [Validators.required]),
 		});
 	}
 }
