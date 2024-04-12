f5b4b01c55cd878e35a9a64f9cf68bc3331e1c68
lucassantossdev <lucas.santos@severinoapp.com>
2024-04-12 19:09:16 -0300

    feat: creating service integration

--- src/app/core/category.service.ts
+++ src/app/core/category.service.ts
@@ -4,20 +4,38 @@ import * as categoryMock from '../shared/mocks/category-mock.json';
 import { Observable, of } from 'rxjs';
 import { CategoryApiInterface } from '../domain/model/category/category-api-interface';
 import { CategoryInterface } from '../domain/model/category/category-interface';
+import { environment } from '../enviroments/enviroment';
+import { CategoryObjectApiInterface } from '../domain/model/category/category-object-api-interface';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class CategoryService {
+	public urlBackEnd = environment.urlBackEnd;
+	constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) { }
+	getAllCategories(): Observable<CategoryApiInterface> {
+		const url = `${this.urlBackEnd}/category`;
+		return this.http.get<CategoryApiInterface>(url);
+	}
 
-  getCategory(): Observable<CategoryApiInterface> {
-    return of(categoryMock);
-  }
+	getCategoryById(id: string): Observable<CategoryObjectApiInterface> {
+		const url = `${this.urlBackEnd}/category/${id}`;
+		return this.http.get<CategoryObjectApiInterface>(url);
+	}
 
-  createCategory(data: CategoryInterface): Observable<CategoryInterface> {
-    const url = 'urlBack'
-    return this.http.post<CategoryInterface>(url, data);
-  }
+	createCategory(data: CategoryInterface): Observable<CategoryInterface> {
+		const url = `${this.urlBackEnd}/category/create`;
+		return this.http.post<CategoryInterface>(url, data);
+	}
+
+	editCategory(id: string, data: CategoryInterface): Observable<CategoryInterface> {
+		const url = `${this.urlBackEnd}/category/${id}`;
+		return this.http.put<CategoryInterface>(url, data);
+	}
+
+	deleteCategory(id: string): Observable<CategoryInterface> {
+		const url = `${this.urlBackEnd}/category/${id}`;
+		return this.http.delete<CategoryInterface>(url);
+	}
 }
--- src/app/features/dashboard/components/patients/category/view/category-view.component.ts
+++ src/app/features/dashboard/components/patients/category/view/category-view.component.ts
@@ -6,6 +6,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { CategoryService } from '../../../../../../core/category.service';
 import { CategoryApiInterface } from '../../../../../../domain/model/category/category-api-interface';
 import { CategoryInterface } from '../../../../../../domain/model/category/category-interface';
+import { CategoryFormComponent } from '../form/category-form.component';
+import { ConfirmationModalComponent } from '../../../../../../shared/modal/confirmation-modal/confirmation-modal.component';
 
 @Component({
 	selector: 'app-category-view',
@@ -15,7 +17,7 @@ import { CategoryInterface } from '../../../../../../domain/model/category/categ
 export class CategoryViewComponent implements OnInit {
 	loading: boolean = false;
 	dataSource!: MatTableDataSource<CategoryInterface>;
-	displayedColumns: string[] = ["image", "name", "description", "action"];
+	displayedColumns: string[] = ["name", "description", "action"];
 	@ViewChild(MatPaginator) paginatior !: MatPaginator;
 	@ViewChild(MatSort) sort !: MatSort;
 
@@ -35,22 +37,56 @@ export class CategoryViewComponent implements OnInit {
 	}
 
 	getAllCategories(): void {
-		this.categoryService.getCategory().subscribe({
+		this.loading = true;
+		this.categoryService.getAllCategories().subscribe({
 			next: (category: CategoryApiInterface) => {
-				this.dataSource = new MatTableDataSource<CategoryInterface>(category.data);
+				setTimeout(() => {
+					this.dataSource = new MatTableDataSource<CategoryInterface>(category.data);
+					this.loading = false;
+				}, 600);
 			}
 		})
 	}
 
 	createCategory(): void {
-		
+		const dialogRef = this.dialog.open(CategoryFormComponent, {
+			width: '800px',
+			height: '200px'
+		});
+
+		dialogRef.afterClosed().subscribe(result => {
+			this.getAllCategories();
+		});
 	}
 
 	editCategory(id: string, data: CategoryInterface): void {
+		const dialogRef = this.dialog.open(CategoryFormComponent, {
+			width: '800px',
+			height: '200px',
+			data: { id: id, category: data },
+		});
 
+		dialogRef.afterClosed().subscribe(result => {
+			this.getAllCategories();
+		});
 	}
 
 	deleteCategory(id: string): void {
+		const dialogRef = this.dialog.open(ConfirmationModalComponent, {
+			width: '300px',
+			data: { title: 'Confirmação', message: 'Deseja realmente excluir?' }
+		});
 
+		dialogRef.afterClosed().subscribe(result => {
+			if (result) {
+				this.categoryService.deleteCategory(id).subscribe({
+					next: () => {
+						this.getAllCategories();
+					}, error: (err: Error) => {
+						console.error('error', err.message);
+					}
+				})
+			}
+		});
 	}
 }
