951e2f2762a1d3b2f002663a87675f3f09abce43
lucassantossdev <lucas.santos@severinoapp.com>
2024-06-18 20:05:22 -0300

    feat: changing services

--- src/app/features/landing-page/components/footer/footer.component.ts
+++ src/app/features/landing-page/components/footer/footer.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  constructor(private router: Router) {
+  }
 
+  ngOnInit(): void {}
+
+  whatsapp(): void {
+    const url = "https://wa.me/5585996112718?text=Ol%C3%A1!%20Vim%20do%20site%2C%20gostaria%20de%20saber%20mais%20sobre%20o%20servi%C3%A7o.";
+    this.router.navigate([url]);
+  }
 }
